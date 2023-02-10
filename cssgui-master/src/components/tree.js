b88d92f8d4b30bbadd41138271bd660bb40a3bdb
herozxb <herozxb@gmail.com>
2023-02-09 17:02:28 -0800

    can show the DIV in the tree view

--- cssgui-master/src/components/tree.js
+++ cssgui-master/src/components/tree.js
@@ -11,7 +11,7 @@ import SortableTree, {
 
 import "@nosferatu500/react-sortable-tree/style.css";
 
-import treeData from "./treeData"
+//import treeData from "./treeData"
 import "./styles.css";
 
 const maxDepth = 5;
@@ -31,6 +31,438 @@ const alertNodeInfo = ({ node, path, treeIndex }) => {
 
 
 
+
+
+
+
+  var json_data = {
+    "nodeType": 1,
+    "tagName": "DIV",
+    "attributes": [
+        {
+            "name": "class",
+            "value": "canvas-panel"
+        }
+    ],
+    "children": [
+        {
+            "nodeType": 1,
+            "tagName": "DIV",
+            "attributes": [
+                {
+                    "name": "class",
+                    "value": "canvas-item"
+                },
+                {
+                    "name": "style",
+                    "value": "background: linear-gradient(90deg, rgb(173, 83, 137) 0%, rgb(60, 16, 83) 100%); width: 500px; height: 500px; top: 246px; left: 1114px; position: absolute; border-color: gray; border-style: solid; border-width: 1px; box-shadow: none; display: flex; justify-content: center; align-items: center; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) skew(0deg); animation-duration: 0.3s; animation-delay: 0s; animation-timing-function: ease; animation-iteration-count: infinite; z-index: 2;"
+                }
+            ],
+            "children": []
+        },
+        {
+            "nodeType": 1,
+            "tagName": "DIV",
+            "attributes": [
+                {
+                    "name": "class",
+                    "value": "canvas-item"
+                },
+                {
+                    "name": "style",
+                    "value": "background: linear-gradient(30deg, rgb(233, 235, 238) 0%, rgb(6, 172, 240) 100%); width: 200px; height: 200px; top: 267px; left: 835px; position: absolute; border-color: gray; border-style: solid; border-width: 1px; box-shadow: grey 5px 5px 20px 0px; display: flex; justify-content: center; align-items: center; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) skew(0deg); animation-duration: 0.3s; animation-delay: 0s; animation-timing-function: ease; animation-iteration-count: infinite; z-index: 2;"
+                }
+            ],
+            "children": []
+        },
+        {
+            "nodeType": 1,
+            "tagName": "DIV",
+            "attributes": [
+                {
+                    "name": "class",
+                    "value": "canvas-item"
+                },
+                {
+                    "name": "style",
+                    "value": "background: linear-gradient(90deg, rgb(255, 165, 0) 0%, rgb(135, 206, 235) 100%); width: 200px; height: 200px; top: 498px; left: 996px; position: absolute; border-color: gray; border-style: solid; border-width: 1px; box-shadow: grey 5px 5px 20px 0px inset; display: flex; justify-content: center; align-items: center; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) skew(0deg); animation-duration: 0.3s; animation-delay: 0s; animation-timing-function: ease; animation-iteration-count: infinite; z-index: 2;"
+                }
+            ],
+            "children": [
+                {
+                    "nodeType": 1,
+                    "tagName": "DIV",
+                    "attributes": [
+                        {
+                            "name": "class",
+                            "value": "selected-element-cursor"
+                        }
+                    ],
+                    "children": []
+                }
+            ]
+        },
+        {
+            "nodeType": 1,
+            "tagName": "DIV",
+            "attributes": [
+                {
+                    "name": "class",
+                    "value": "footer"
+                }
+            ],
+            "children": [
+                {
+                    "nodeType": 3,
+                    "tagName": "a",
+                    "textContent": "Created by "
+                },
+                {
+                    "nodeType": 1,
+                    "tagName": "A",
+                    "attributes": [
+                        {
+                            "name": "href",
+                            "value": "https://twitter.com/li_guangyi"
+                        },
+                        {
+                            "name": "target",
+                            "value": "_blank"
+                        }
+                    ],
+                    "children": [
+                        {
+                            "nodeType": 3,
+                            "tagName": "a",
+                            "textContent": "Guangyi Li"
+                        }
+                    ]
+                },
+                {
+                    "nodeType": 3,
+                    "tagName": "a",
+                    "textContent": " / "
+                },
+                {
+                    "nodeType": 1,
+                    "tagName": "A",
+                    "attributes": [
+                        {
+                            "name": "href",
+                            "value": "mailto:liguangyi08@gmail.com"
+                        }
+                    ],
+                    "children": [
+                        {
+                            "nodeType": 3,
+                            "tagName": "a",
+                            "textContent": "Email Me"
+                        }
+                    ]
+                },
+                {
+                    "nodeType": 3,
+                    "tagName": "a",
+                    "textContent": "  / "
+                },
+                {
+                    "nodeType": 1,
+                    "tagName": "A",
+                    "attributes": [
+                        {
+                            "name": "href",
+                            "value": "https://github.com/hh54188/cssgui"
+                        },
+                        {
+                            "name": "target",
+                            "value": "_blank"
+                        }
+                    ],
+                    "children": [
+                        {
+                            "nodeType": 3,
+                            "tagName": "a",
+                            "textContent": "Source Code"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+
+
+
+
+
+const data = JSON.parse( JSON.stringify( json_data ) );
+
+var id = 0
+
+function mapDataToTreeData(data) {
+
+    var node = {}
+
+    for (let key in data) 
+    {
+      var value = data[key];
+
+      if (key ==="nodeType") {
+        node.nodeType = 1
+        node.title = "DIV"
+
+      }
+
+      if (key ==="tagName") {
+        node.tagName = value
+      }
+
+      if (key ==="attributes") {
+        node.attributes = value
+        node.subtitle = value[0]["name"] + ":" + value[0]["value"] 
+      }
+
+      if (key ==="textContent") {
+
+        node.nodeType = 3
+        node.title = "TEXT"
+        node.subtitle = value
+        node.textContent = value
+
+        return node
+
+      }
+
+      
+
+      if (key ==="children") {
+        var treeData = [];
+
+        if (typeof value === "object") {
+          var children = []
+
+          for (let i = 0; i < value.length; i++) {
+            children.push(mapDataToTreeData(value[i]));
+          }
+
+          node.children = children
+
+          if (value.length==0) {
+
+          }
+          
+          
+        } else {
+
+          node.children = [];
+          
+        }
+
+
+        return node
+
+
+      }
+   
+    }
+    
+  }
+
+
+
+function treeDataToJson(data) {
+
+    var node = {}
+
+    for (let key in data) 
+    {
+      //console.log("=============key===========")
+      //console.log(key)
+      var value = data[key];
+      //console.log(value)
+
+
+      if (key ==="nodeType") {
+        node.nodeType = value
+        //console.log("======nodeType======")
+        //console.log(value)
+      }
+
+      if (key ==="tagName") {
+        node.tagName = value
+        //console.log("======tagName======")
+        //console.log(value)
+      }
+
+      if (key ==="attributes") {
+        node.attributes = value
+        //console.log("======attributes======")
+        //console.log(value)
+      }
+
+      if (key ==="textContent") {
+
+        node.nodeType = 3
+        node.textContent = value
+
+        //console.log("======textContent======")
+        //console.log(value)
+        
+        //console.log("==========children_node==========")
+        //console.log(node)
+        return node
+
+      }
+
+      
+      
+
+      if (key ==="children") {
+        var treeData = [];
+
+        if (typeof value === "object") {
+          var children = []
+          //console.log("==========children_1==========")
+          //console.log(value)
+
+          for (let i = 0; i < value.length; i++) {
+            children.push(treeDataToJson(value[i]));
+          }
+
+          node.children = children
+          //console.log("==========children_2===========")
+          //console.log(children)
+
+          if (value.length==0) {
+
+          }
+          
+          
+        } else {
+
+          node.children = [];
+          
+        }
+
+
+        //console.log("==========children_node==========")
+        //console.log(node)
+        return node
+
+
+      }
+      //*/
+   
+    }
+    
+  }
+
+const treeData = [mapDataToTreeData(data)];
+
+console.log("======3=======")
+console.log( treeData )
+
+var rejson = treeDataToJson(treeData[0]) 
+console.log("==========back_json============")
+//console.log( rejson )
+console.log( JSON.stringify(converter(rejson), null, 4) )
+
+function converter(dom) {
+
+
+    //console.log("===========converter===========");
+    //console.log(dom)
+    const obj = {};
+    if (dom.nodeType === Node.TEXT_NODE) {
+        obj.nodeType = 3
+        obj.tagName = "a"
+        obj.textContent = dom.nodeValue;
+        //console.log("=========TEXT_NODE========")
+        //console.log(obj)
+        return obj;
+    }
+    if (dom.nodeType === Node.DOCUMENT_NODE) {
+        ////console.log("document")
+        dom = dom.documentElement;
+    }
+
+    obj.nodeType = dom.nodeType;
+    if (dom.nodeType === Node.ELEMENT_NODE) {
+
+        //console.log("=========ELEMENT_NODE========")
+        //console.log(dom)
+
+        obj.tagName = dom.tagName;
+        obj.attributes = []; // Array.from(obj.attributes) gives us a lot of things we don't want
+        for (let i = 0, len = dom.attributes.length; i < len; ++i) {
+            const attr = dom.attributes[i];
+            obj.attributes.push({name: attr.name, value: attr.value});
+        }
+        obj.children = [];
+        //console.log("=============dom_children===============")
+        //console.log(dom.children)
+        for (let i = 0; i < dom.children.length; ++i)  {
+            //console.log("=========for===========")
+            //console.log(dom.children[i])
+            obj.children.push(converter(dom.children[i]));
+        }
+        
+        //console.log("===========after============")
+        //console.log(obj)
+
+    } else {
+        obj.nodeType = 3
+        obj.tagName = "a"
+        obj.textContent = dom.nodeValue;
+    }
+    return obj;
+}
+
+const HtmlNode = (tagName, attributes = [], children = []) => {
+  const e = document.createElement(tagName)
+  for (const attribute of attributes) { e.setAttribute(attribute.name, attribute.value); }
+  for (const child of children) e.appendChild(toNode(child))
+  return e
+}
+
+const TextNode = (text) => {
+  return document.createTextNode(text)
+}
+  
+const toNode = t => {
+  switch (t?.nodeType) {
+    case 1: return HtmlNode(t.tagName, t.attributes, t.children)
+    case 3: return TextNode(t.textContent)
+  }
+}
+
+const json2html = json => toNode(JSON.parse(json))
+
+
+
+function get_dev_content()
+{
+  var MyDiv1 = document.getElementsByClassName('canvas-panel')[0];
+  ////console.log(MyDiv1.innerHTML)
+  console.log("===================DOM=====================");
+  //console.log(MyDiv1)
+  const json = JSON.stringify(converter(MyDiv1), null, 4);
+  //console.log(json);
+  ////console.log(json2html(json));
+  document.getElementsByTagName('body')[0].appendChild(   json2html( JSON.stringify( converter( rejson ), null, 4) ) );
+
+}
+
+function change_dev_content(treeData)
+{
+
+  document.getElementsByTagName('body')[0].appendChild(   json2html( JSON.stringify( converter( treeDataToJson(treeData[0]) ), null, 4) ) );
+
+}
+
+
+
 class Tree extends React.Component {
   state = {
     searchString: "",
@@ -41,6 +473,9 @@ class Tree extends React.Component {
 
   handleTreeOnChange = (treeData) => {
     this.setState({ treeData });
+    console.log("================change================")
+    console.log(treeData)
+    change_dev_content(treeData)
   };
 
   handleSearchOnChange = (e) => {
@@ -92,7 +527,7 @@ class Tree extends React.Component {
     return (
       <div className="wrapper_tree">
         <div className="bar_wrapper_tree">
-
+          <button onClick={() => get_dev_content()} >New Dev</button>
           <button onClick={this.toggleNodeExpansion.bind(this, true)}>
             Expand all
           </button>
